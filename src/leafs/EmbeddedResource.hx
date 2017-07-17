b8eea4d6f6b5d8406defb41e2e9f69bf49a1f56a
azrafe7 <azrafe7@gmail.com>
2017-07-17 16:02:52 +0200

    update EmbeddedResource

--- src/leafs/EmbeddedResource.hx
+++ src/leafs/EmbeddedResource.hx
@@ -4,33 +4,45 @@ import haxe.Resource;
 import haxe.io.Bytes;
 
 
-/** Represents a named embedded resource, with lazy loading from haxe.Resource when accessed `asString` or `asBytes`. */
+/** 
+ * Represents a named embedded resource, with optional metadata.
+ * 
+ * It will be lazy loaded using `haxe.Resource.getString/Bytes()` when accessing the `asString` and `asBytes` props,
+ * and cached from there on. 
+ */
 class EmbeddedResource {
   
   public var name(default, null):String;
   
-  public var asString(get, null):String;
+  public var metadata:Dynamic = null;
+  
   var _asString:String;
+  public var asString(get, null):String;
   inline function get_asString():String {
     if (_asString == null) {
-      trace("was null");
       _asString = Resource.getString(name);
     }
     return _asString;
   }
   
-  public var asBytes(get, null):Bytes;
   var _asBytes:Bytes;
+  public var asBytes(get, null):Bytes;
   inline function get_asBytes():Bytes {
     if (_asBytes == null) {
-      trace("was null");
       _asBytes = Resource.getBytes(name);
     }
     return _asBytes;
   }
   
   
-  public function new(name:String) {
+  public function new(name:String, ?metadata:Dynamic) {
     this.name = name;
+    this.metadata = metadata;
+  }
+  
+  /** Dispose of this resource (just sets `asString` and `asBytes` to null). */
+  public function dispose():Void {
+    this._asString = null;
+    this._asBytes = null;
   }
 }
