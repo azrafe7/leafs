5a02148e8b1a634a3437f058f6a5395e7aa96373
azrafe7 <azrafe7@gmail.com>
2017-09-12 21:52:25 +0200

    update AutoCompleteExample

--- src/AutoCompleteExample.hx
+++ src/AutoCompleteExample.hx
@@ -9,6 +9,9 @@ class AutoCompleteExample {
   static public function main() {
     trace(AllAssets + "\n");
     trace(AllAssetsAnon + "\n");
+    trace(Msg + "\n");
+    trace(Other + "\n");
+    trace(Multi + "\n");
   }
 }
 
@@ -18,4 +21,15 @@ class AllAssets { }
 
 @:build(leafs.AutoComplete.fromFSIntoAnon("assets", true))
 class AllAssetsAnon { }
+
+@:build(leafs.AutoComplete.fromFSIntoAnon("assets", true, {fsFilter:"FILES_ONLY", regexPattern:"[.]txt$", regexOptions:"gi"}, "messages"))
+class Msg { }
+
+@:build(leafs.AutoComplete.fromFSIntoAnon("assets", true, {fsFilter:"ANY", regexPattern:"other", regexOptions:"gi"}, "other"))
+class Other { }
+
+@:build(leafs.AutoComplete.fromFSIntoAnon("assets", true, {fsFilter:"FILES_ONLY", regexPattern:"[.]zip$", regexOptions:"gi"}, "zip"))
+@:build(leafs.AutoComplete.fromFSIntoAnon("assets", true, {fsFilter:"DIRS_ONLY"}, "dirs"))
+@:build(leafs.AutoComplete.fromFSIntoAnon("assets", true, {fsFilter:"FILES_ONLY", regexPattern:"[.]txt$", regexOptions:"gi"}, "txt"))
+class Multi { }
 
